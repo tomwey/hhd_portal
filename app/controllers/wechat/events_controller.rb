c16d4a122352b63247e3a7c315c17bc2847eb5f2
tomwey <tomwey@163.com>
2018-04-11 13:57:29 +0800

    ccc

--- app/controllers/wechat/events_controller.rb
+++ app/controllers/wechat/events_controller.rb
@@ -54,14 +54,14 @@ class Wechat::EventsController < Wechat::ApplicationController
   
   def share
     @event_portal = "#{SiteConfig.hhd_event_portal}/wx/event/portal?" + request.query_string
-    
-    request.headers['foo'] = 'bar'
+    #
+    # request.headers['foo'] = 'bar'
     # url = request.original_url#"#{SiteConfig.hhd_event_portal}/wx/event/portal?" + request.original_url.split('?').last
     #
     # redirect_url  = "#{SiteConfig.wx_auth_redirect_uri}?url=#{url}"#"#{wechat_auth_redirect_url}?url=#{request.original_url}"
     #
     # @wechat_auth_url = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=#{SiteConfig.wx_app_id}&redirect_uri=#{Rack::Utils.escape(redirect_url)}&response_type=code&scope=snsapi_userinfo&state=yujian#wechat_redirect"
-    redirect_to @event_portal
+    # redirect_to @event_portal
   end
   
   # 活动参与结果
